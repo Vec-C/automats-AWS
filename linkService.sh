e69ab7ed778d86382c31eec63ef70bb804c796e0
René <rene.tapia@alsea.com.mx>
2021-10-11 17:09:36 -0500

    Link Service v0.1

--- linkService.sh
+++ linkService.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 #****************Reading Swagger File with New Endpoints ***********
-
 helpFunction()
 {
    echo ""
@@ -10,8 +9,7 @@ helpFunction()
    exit 1 # Exit script after printing help
 }
 
-while getopts "a:" opt
-do
+while getopts "a:" opt; do
    case "$opt" in
       a ) SWAGGER="$OPTARG" ;;
       ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
@@ -26,8 +24,7 @@ then
 fi
 
 #****************Found Resources on Certain API***********
-
-#****************APIS***********
+#****************LIST APIS***********
 aws apigateway get-rest-apis --max-items 100 > apis.json
 echo $(sed '/policy/d' apis.json) > apis.json
 
@@ -50,6 +47,7 @@ RESOURCES=$(aws apigateway get-resources --rest-api-id $APIID)
 
 echo $RESOURCES | jq '.items[] | "\(.id) \(.path) \(.resourceMethods)\n"' > linkrecs.txt
 
+#***********SEEK METHODS FOR EACH RESOURCE**********
 NEWRESOURCES=$(jq -r -f list-operations.jq $SWAGGER)
 
 declare -a CREATER
@@ -59,15 +57,17 @@ IFS='
 count=0
 cflag=0
 
-for item in $NEWRESOURCES
-do
+#***********VALIDATING PREXISTENCE OF REQUESTED OBJECTS FOR CREATION**********
+for item in $NEWRESOURCES; do
 
   if [ "hearbeat" != "$(echo $item | sed -En 's/^([a-z]+)&&\/(.*)&&(.*)$/\2/p')" ]
   	then
 		MET=$(echo $item | sed -En 's/^([a-z]+)&&\/(.*)&&(.*)$/\1/p')
 		REC=$(echo $item | sed -En 's/^([a-z]+)&&\/(.*)&&(.*)$/\2/p')
+		REC=$(if [ -z "$(sed -En 's/\//\\\//gp' <<< $REC)" ] ;then echo $REC ; else sed -En 's/\//\\\//gp' <<< $REC; fi )
 		if [ -z "$(sed -En 's/^.*('$REC')[[:blank:]]+.*(('$MET').*)+$/\1 \2/pi' linkrecs.txt)" ]
 			then
+				REC=$(echo $REC | tr -d '\\')
 				DEPLOY=$DEPLOY$REC" "$MET"\n"
 				CREATER[cflag]=$REC
 				CREATEM[cflag]=$MET
@@ -81,7 +81,6 @@ do
 done
 
 IFS=" "
-
 if [ $count == 0 ]
 	then 
 		echo "Do u wanna deploy the next resources:\n"$DEPLOY"?"
@@ -90,78 +89,102 @@ if [ $count == 0 ]
 	else
 		exit
 fi
-
 if [ $DOIT == "yes" ]
 	then 
 		DEPLOY=""
-		PARENT=$(sed -En 's/"([a-z0-9]+) \/ null.*/\1/p' linkrecs.txt)
+		PARENT=$(sed -En 's/"([a-z0-9]+) \/ .*/\1/p' linkrecs.txt)
 		declare -a PARENTS
 		declare -a CHILDS
 		count=0
 		PARENTS[$count]="qwerty "$PARENT
 		count=$((count+1))
 
-		for i in ${!CREATER[@]}
-		do
+		#***********FOR EACH RESOURCE**********
+		for i in ${!CREATER[@]}; do
 
 			IFS='/'
 			countI=0
 
-			for j in ${CREATER[$i]}
-			do
+			#***********FOR EACH RESOURCE CHILD/VERB ( /... )**********
+			for j in ${CREATER[$i]}; do
 
 				IFS=" "
-
 				if [[ ! " ${PARENTS[*]} " =~ " $j " ]]; then
 					if [ $countI == 0 ]
-					then
-						count=1
+						then
+							count=1
 					fi
 					echo "Create resource: "$j
-					PN=$( echo ${PARENTS[$((count-1))]} | ggrep -Po '^.*(?=\s)' )
-					echo $PN
 					PID=$( echo ${PARENTS[$((count-1))]} | ggrep -Po '(?<=\s).*$' )
-					echo $PID
-					PA=$(aws apigateway create-resource --rest-api-id $APIID --parent-id $PID --path-part $j)
-					PARENTS[$count]=$j" "$(echo $PA | jq .id | tr -d '"')
+					PA=$(aws apigateway create-resource --rest-api-id $APIID --parent-id $PID --path-part $j || sed -En 's/"([a-z0-9]+) \/'$j' .*/\1/p' linkrecs.txt)
+					if [ -z "$(echo $PA | jq .id )" ] 
+					then
+						PARENTS[$count]=$j" "$PA
+					else
+						PARENTS[$count]=$j" "$(echo $PA | jq .id | tr -d '"')
+					fi
 					count=$((count+1))
 					countI=$((countI+1))
 				else
+
 					for key in "${!PARENTS[@]}"; do
+
 				   	if [[ $( echo ${PARENTS[$key]} | ggrep -Po '^.*(?=\s)' ) == "$j" ]]; then
 				      	PARENTS[$count]=${PARENTS[$key]}
 				      	count=$((count+1))
 				      	countI=$((countI+1))
 				   	fi
+
 					done
+				
 				fi
 
 			done
 
 			echo "Creating "${CREATEM[$i]}" method for /"${CREATER[$i]}
-			echo "Header parameters:"
-			jq '.paths["/'${CREATER[$i]}'"] | .'${CREATEM[$i]}' | .parameters[]? | select(.required == false) | select(.in=="header")' $SWAGGER
-			echo "Query parameters:"
-			jq '.paths["/'${CREATER[$i]}'"] | .'${CREATEM[$i]}' | .parameters[]? | select(.required == false) | select(.in=="query")' $SWAGGER
+			HPARAMS=$( jq -r '.paths["/'${CREATER[$i]}'"] | .'${CREATEM[$i]}' | .parameters[]? | select(.required == false) | select(.in=="header" ) | .name ' $SWAGGER )
+			QPARAMS=$( jq -r '.paths["/'${CREATER[$i]}'"] | .'${CREATEM[$i]}' | .parameters[]? | select(.required == false) | select(.in=="query" ) | .name ' $SWAGGER )
+			CODERESPONSES=$( jq -r '.paths["/'${CREATER[$i]}'"] | .'${CREATEM[$i]}' | .responses | to_entries | .[] | .key' $SWAGGER )
+			RESOURCES=$(aws apigateway get-resources --rest-api-id $APIID)
+			echo $RESOURCES | jq '.items[] | "\(.id) \(.path) \(.resourceMethods)\n"' > linkrecs.txt
+			REC=$(if [ -z "$(sed -En 's/\//\\\//gp' <<< ${CREATER[$i]})" ] ;then echo ${CREATER[$i]} ; else sed -En 's/\//\\\//gp' <<< ${CREATER[$i]}; fi )
+			RID=$( sed -En 's/"([a-z0-9]+) \/'$REC' .*/\1/p' linkrecs.txt )
+			METHOD=$( echo ${CREATEM[$i]} | tr [:lower:] [:upper:] )
+			RPARAMETERS=""
+			RIPARAMETERS=""
+
+			while read p; do    
+				if [ ! -z $p ] ; then
+  					RPARAMETERS=$RPARAMETERS"method.request.header.$p=false,"
+  					RIPARAMETERS=$RIPARAMETERS"integration.request.header.$p=method.request.header.$p,"
+  				fi
+			done <<< $HPARAMS
+
+			while read p; do  
+				if [ ! -z $p ] ; then  
+  					RPARAMETERS=$RPARAMETERS"method.request.querystring.$p=false,"
+  					RIPARAMETERS=$RIPARAMETERS"integration.request.querystring.$p=method.request.querystring.$p,"
+				fi
+			done <<< $QPARAMS
+
+			if [ ! -z $RPARAMETERS ] ; then
+				aws apigateway put-method --rest-api-id $APIID --resource-id $RID --http-method $METHOD --authorization-type "NONE" --no-api-key-required --request-parameters "${RPARAMETERS%?}"
+				aws apigateway put-integration --rest-api-id $APIID --resource-id $RID --http-method $METHOD --type HTTP --integration-http-method $METHOD --uri 'http://${stageVariables.'${SWAGGER%?????}'}/'${CREATER[$i]} --request-parameters "${RIPARAMETERS%?}"
+			else
+				aws apigateway put-method --rest-api-id $APIID --resource-id $RID --http-method $METHOD --authorization-type "NONE" --no-api-key-required
+				aws apigateway put-integration --rest-api-id $APIID --resource-id $RID --http-method $METHOD --type HTTP --integration-http-method $METHOD --uri 'http://${stageVariables.'${SWAGGER%?????}'}/'${CREATER[$i]}
+			fi
+
+			#***********ADDING RESPONSE CODES**********
+			while read p; do  
+				if [ ! -z $p ] ; then  
+  					aws apigateway put-method-response --rest-api-id $APIID --resource-id $RID --http-method $METHOD --status-code $p
+					aws apigateway put-integration-response --rest-api-id $APIID --resource-id $RID --http-method $METHOD --status-code $p --selection-pattern $p
+				fi
+			done <<< $CODERESPONSES
 
 		done
 	
 	else
 		exit
 fi
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
