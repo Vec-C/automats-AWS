6f14054b9270a37c054ecd07924d1968b0c46981
René <rene.tapia@alsea.com.mx>
2022-04-04 12:06:49 -0500

    Updated

--- cloneECR.sh
+++ cloneECR.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 TARGET_ACCOUNT_REGION=us-east-1
 DESTINATION_ACCOUNT_REGION=us-east-1
-DESTINATION_ACCOUNT_BASE_PATH=624694691300.dkr.ecr.$DESTINATION_ACCOUNT_REGION.amazonaws.com
+DESTINATION_ACCOUNT_BASE_PATH=xxxxxxxxxxx.dkr.ecr.$DESTINATION_ACCOUNT_REGION.amazonaws.com
 
 #REPO_LIST=($(aws --profile core-prod ecr describe-repositories --query 'repositories[].repositoryUri' --output text --region $TARGET_ACCOUNT_REGION))
-REPO_LIST=(470036567262.dkr.ecr.us-east-1.amazonaws.com/amazoncorretto)
+REPO_LIST=(xxxxxxxxxx.dkr.ecr.us-east-1.amazonaws.com/amazoncorretto)
 
 for repo_url in ${REPO_LIST[@]}; do
     NAME=$( ggrep -Po '(?<=/)(.*)$' <<< $repo_url )
